b393d758e79e90a098d3a5fe6058effc098f63b0
peachomon <saharov.dev@gmail.com>
2023-07-24 16:51:45 +0300

    New player controller

--- Potion Shoplift/Assets/Scripts/PlayerController.cs
+++ Potion Shoplift/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,36 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+ 
+public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-
-    private Rigidbody2D rb;
-    private Vector2 moveVelocity;
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
+    //I recommend 7 for the move speed, and 1.2 for the force damping
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 forceToApply;
+    public Vector2 PlayerInput;
+    public float forceDamping;
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput.normalized * speed;
+        PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
-
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        Vector2 moveForce = PlayerInput * moveSpeed;
+        moveForce += forceToApply;
+        forceToApply /= forceDamping;
+        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+        {
+            forceToApply = Vector2.zero;
+        }
+        rb.velocity = moveForce;
+    }
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            forceToApply += new Vector2(-20, 0);
+            Destroy(collision.gameObject);
+        }
     }
 }
