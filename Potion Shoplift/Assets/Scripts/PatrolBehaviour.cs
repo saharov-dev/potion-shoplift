ea81b6aab77e47d6c57204d72f83fddbbefe7a55
peachomon <saharov.dev@gmail.com>
2023-08-07 13:43:48 +0300

    pup

--- Potion Shoplift/Assets/Scripts/PatrolBehaviour.cs
+++ Potion Shoplift/Assets/Scripts/PatrolBehaviour.cs
@@ -4,33 +4,38 @@ using UnityEngine;
 
 public class PatrolBehaviour : StateMachineBehaviour
 {
+    private PatrolSpots patrol;
+    public float speed;
+    private int randomSpot;
+
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        patrol = GameObject.FindGameObjectWithTag("PatrolSpots").GetComponent<PatrolSpots>();
+        randomSpot = Random.Range(0, patrol.patrolPoints.Length);
     }
+
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Vector2.Distance(animator.transform.position, patrol.patrolPoints[randomSpot].position) > 0.2f)
         {
-            animator.SetBool("isPatroling", false);
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, patrol.patrolPoints[randomSpot].position, speed * Time.deltaTime);
+
         }
+        else
+        {
+            randomSpot = Random.Range(0, patrol.patrolPoints.Length);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetBool("isPatrolling", false);
+        }
+
     }
+
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
